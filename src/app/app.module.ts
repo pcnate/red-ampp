5a8ce317f614aee1ebb3f44670c0ac2d624d5b21
Nathan Baker <pcnate@gmail.com>
2018-11-03 02:05:15 -0500

    adding forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { SettingsComponent } from './settings/settings.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { SettingsComponent } from './settings/settings.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     TooltipModule.forRoot()
   ],
   providers: [],
