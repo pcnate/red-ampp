4cef2b7376b4f03ba25bb41a6662656655be5e68
Nathan Baker <pcnate@gmail.com>
2018-11-03 09:00:06 -0500

    add logs and status, fix routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { SettingsComponent } from './settings/settings.component';
+import { StatusComponent } from './status/status.component';
 import { FormsModule } from '@angular/forms';
+import { LogsComponent } from './logs/logs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    SettingsComponent
+    SettingsComponent,
+    StatusComponent,
+    LogsComponent
   ],
   imports: [
     BrowserModule,
