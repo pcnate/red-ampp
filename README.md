f7034c778437669f7e00b4b570aae047aef6d0fb
Nathan Baker <pcnate@gmail.com>
2018-11-03 20:58:40 -0500

    merge old app stuff back

--- README.md
+++ README.md
@@ -1,27 +1,8 @@
-# RedAmppGui
+# redapp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.0.4.
+Developer tool for running micro services behind a redbird proxy.
 
-## Development server
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+>__WARNING:__
+>
+>This is only meant to run as a development tool on localhost. Please don't make it run on an external facing interface without added some security.
---
+++ server.js
@@ -0,0 +1,114 @@
+require('dotenv').config();
+
+const proxy = require('./proxy');
+const app = require('express')();
+const http = require('http').Server( app );
+const bodyParser = require('body-parser');
+
+const storage = require('node-persist');
+storage.init();
+
+const baseAPI = '/red-ampp/api/';
+var port = 0;
+var address = 0;
+
+// app use stuff here
+app.use( bodyParser() );
+
+// start the proxy
+proxy.start().then( () => {
+  
+  listener = http.listen(() => {
+
+    // save for status and later usage
+    port = listener.address().port;
+    address = listener.address().address;
+
+    console.log( 'management app listening on port', port );
+    
+    // if in development, load angular from port 4200
+    const gui_port = process.env.NODE_ENV !== 'production' ? 4200 : port;
+    
+    // angular development mode
+    if( process.env.NODE_ENV !== 'production' ) {
+      proxy.register( '/sockjs-node', 'localhost:' + gui_port + '/sockjs-node', false )
+      .then(() => {})
+      .catch(error => {
+        console.error( 'unknown error binding angular sockjs_node' );
+      });
+    }
+
+    // bind the management portal
+    proxy.register( '/red-ampp', 'localhost:' + gui_port, false )
+      .then( () => {
+        console.log( 'access the management portal at http://localhost/red-ampp' );
+      })
+      .catch( error => {
+        console.error( 'unknown error binding the management portal' );
+      });
+    proxy.register( '/red-ampp/api', 'localhost:' + port + '/red-ampp/api', false )
+      .then( () => {})
+      .catch( error => {
+        console.error( 'unknown error binding the management portal api' );
+      });
+
+    storage.forEach( async datum => {
+      let [ path, destination ] = JSON.parse( datum.key );
+      await proxy.register( path, destination, datum.value );
+    })
+  });
+
+});
+
+// listen for a few paths
+app.get('/', ( request, response ) => {
+  response.send('success');
+});
+
+app.get( baseAPI + 'getRoutes', ( request, response ) => {
+  response.send( proxy.getRoutes() );
+});
+
+app.post( baseAPI + 'register', ( request, response ) => {
+  proxy.register( request.body.path, request.body.destination )
+  .then( async() => {
+    let item = JSON.stringify( [ request.body.path, request.body.destination ] );
+    await storage.setItem( item, true );
+    response.send({
+      success: true
+    });
+  })
+  .catch( error => {
+    response.status(500).send({
+      sucess: false,
+    })
+  })
+});
+app.post( baseAPI + 'unregister', ( request, response ) => {
+  proxy.unregister( request.body.path, request.body.destination )
+  .then( async() => {
+    let item = JSON.stringify( [ request.body.path, request.body.destination ] );
+    await storage.removeItem( item, true );
+    response.send({
+      success: true
+    });
+  })
+  .catch( error => {
+    response.status(500).send({
+      sucess: false,
+    })
+  })
+});
+
+app.get( baseAPI + 'status', ( request, response ) => {
+  let status = {
+    address,
+    port,
+    proxy: proxy ? true : false,
+    requests: proxy.getRequestStats(),
+    PID: process.pid,
+    redbirdPID: proxy.getRedbirdPID(),
+  };
+
+  response.send( status );
+});
