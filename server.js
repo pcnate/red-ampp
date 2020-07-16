b57837516e16f0cf80f464abef4469f195eb16a6
Nathan Baker <nbaker@boxlogixautomation.com>
2020-07-16 14:37:41 -0500

    check for production mode

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+require('redbird');
 const proxy      = require('./app/proxy');
 const express    = require('express');
 const path       = require('path');
@@ -9,6 +10,10 @@ const bodyParser = require('body-parser');
 const io         = require('socket.io')( http, {
   path: '/socket.io/',
 });
+
+if ( !process.env.NODE_ENV && process.pkg ) {
+  process.env.NODE_ENV = 'production';
+}
 
 const storage = require('node-persist');
 storage.init({
