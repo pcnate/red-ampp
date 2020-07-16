9c4f324c969572619347f479e04ca1c9989dd926
Nathan Baker <nbaker@boxlogixautomation.com>
2020-07-16 14:37:13 -0500

    file utilities

--- bin/utils.js
+++ bin/utils.js
@@ -1,5 +1,6 @@
 const os = require('os');
-var fs = require('fs');
+const path = require('path');
+var fs = require('fs-extra');
 var cli = require("cli-color");
 
 const log   = ( ...a ) => console.log( a.join(' ') );
@@ -13,16 +14,40 @@ const header = ( ...a ) => log( [ os.EOL, '#'.repeat( 40 ), os.EOL, os.EOL, '  '
  * @param {string} source file
  * @param {string} destination file
  */
-function copyFile( source, destination ) {
-  info( 'cp', source, destination );
+function copyFile( source, destination, silent=false ) {
+  if( !silent )
+    info( 'cp', source, destination );
   return new Promise( async resolve => {
     await fs.createReadStream( source ).pipe( fs.createWriteStream( destination ) );
     resolve();
   })
 }
 
+/**
+ * copy a folder
+ *
+ * @param {string} source folder
+ * @param {string} destination folder
+ */
+function copyFolder( source, destination ) {
+  info( 'cp', source, destination );
+  return new Promise( async resolve => {
+
+    await fs.ensureDirSync( destination );
+    dirs = fs.readdirSync( source )
+
+    for( dir of dirs ) {
+      await copyFile( path.join( source, dir ), path.join( destination, dir ), true )
+    }
+
+    // await fs.createReadStream( source ).pipe( fs.createWriteStream( destination ) );
+    resolve();
+  })
+}
+
 module.exports = {
   copyFile,
+  copyFolder,
   log,
   info,
   error,
